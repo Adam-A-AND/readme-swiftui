89fdab38e66d73a0db6352cbc4942dd3dd617ed3
Adam-A-AND <adam.armstrong@and.digital>
2022-08-26 12:05:25 +0100

    Initial commit

---
+++ readme/readmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  readmeApp.swift
+//  readme
+//
+//  Created by Adam Armstrong on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct readmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
