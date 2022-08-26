b88611fd741845542edf3f165f8e3516c9de842c
Adam-A-AND <adam.armstrong@and.digital>
2022-08-26 17:00:12 +0100

    feat(Task 2): Create data flow between screens

--- readme/Views/DetailView.swift
+++ readme/Views/DetailView.swift
@@ -6,21 +6,66 @@
 //
 
 import SwiftUI
+import class PhotosUI.PHPickerViewController
 
 struct DetailView: View {
-    let book: Book
+    @Binding var image: Image?
+    @State var imagePickerIsShowing = false
+    @State var deleteImageDialogIsShowing = false
+    @ObservedObject var book: Book
+    
     var body: some View {
         VStack (alignment: .leading) {
-            TitleAndAuthorView(book: book, titleFont: .title, authorFont: .title2)
-            Book.Image(title: book.title)
+            HStack (spacing: 16) {
+                Button(action: {
+                    book.readMe.toggle()
+                }, label: {
+                    Image(systemName: book.readMe ? "bookmark.fill" : "bookmark")
+                })
+                TitleAndAuthorView(book: book, titleFont: .title, authorFont: .title2)
+            }
+            
+            VStack {
+                Divider()
+                    .padding(.vertical)
+                TextField("Review...", text: $book.microReview)
+                Divider()
+                    .padding(.vertical)
+                Book.Image(image: image, title: book.title, cornerRadius: 16.0)
+                    .scaledToFit()
+                HStack {
+                    if (image != nil) {
+                        Spacer()
+                        Button("Delete Image") {
+                            deleteImageDialogIsShowing = true
+                        }
+                    }
+                    Spacer()
+                    Button("Update Image...") {
+                        imagePickerIsShowing = true
+                    }
+                    Spacer()
+                }
+                .padding()
+            }
+            .padding()
             Spacer()
         }
         .padding()
+        .sheet(isPresented: $imagePickerIsShowing) {
+            PHPickerViewController.View(image: $image)
+        }
+        .confirmationDialog("Delete image for \(book.title)", isPresented: $deleteImageDialogIsShowing) {
+            Button("Delete", role: .destructive) {image = nil }
+        } message: {
+            Text("Delete image for \(book.title)?")
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(book: .init())
+        DetailView(image: .constant(nil), book: .init())
+            .previewedInAllColorSchemes
     }
 }
