334e0ab3b4eb43af6009a3db1467cf9db2c2d554
Adam Armstrong <65761327+Adam-A-AND@users.noreply.github.com>
2022-09-05 08:54:15 +0100

    Develop (#4)
    * feat(Task 1): setup basic UI (#1)
    
    - Setup book list view
    - Setup book detail view
    - Setup navigation between list & detail views
    - Add example book library data
    
    * feat(Task 2): Create data flow between screens (#2)
    
    - Adds functionality to add & remove images for each book
    - Adds confirmation dialog when removing books
    
    * feat(Task 3): Add new book (#3)
    
    - Adds row content for all books in library
    - Adds functionality to add new books to the library
    - Creates sections to organise the books that have been read and one's that haven't
    - Adds swipe action functionality to mark books as read
    - Adds swipe action functionality to delete a book from the library

--- readme/Library.swift
+++ readme/Library.swift
@@ -32,12 +32,29 @@
 
 import SwiftUI
 
-@main
-struct ReadMeApp: App {
-  var body: some Scene {
-    WindowGroup {
-      ContentView()
-        .environmentObject(Library())
-    }
+class Library: ObservableObject {
+  var sortedBooks: [Book] { booksCache }
+  
+  /// Adds a new book at the start of the library's manually-sorted books.
+  func addNewBook(_ book: Book, image: Image?) {
+    booksCache.insert(book, at: 0)
+    images[book] = image
   }
+
+  /// An in-memory cache of the manually-sorted books.
+  @Published private var booksCache: [Book] = [
+    .init(title: "Ein Neues Land", author: "Shaun Tan"),
+    .init(title: "Bosch", author: "Laurinda Dixon", microReview: "Earthily Delightful."),
+    .init(title: "Dare to Lead", author: "Brené Brown"),
+    .init(title: "Blasting for Optimum Health Recipe Book", author: "NutriBullet"),
+    .init(title: "Drinking with the Saints", author: "Michael P. Foley", microReview: "One of Ozma's favorites! 😻"),
+    .init(title: "A Guide to Tea", author: "Adagio Teas"),
+    .init(title: "The Life and Complete Work of Francisco Goya", author: "P. Gassier & J Wilson", microReview: "Book too large for a micro-review!"),
+    .init(title: "Lady Cottington's Pressed Fairy Book", author: "Lady Cottington"),
+    .init(title: "How to Draw Cats", author: "Janet Rancan"),
+    .init(title: "Drawing People", author: "Barbara Bradley"),
+    .init(title: "What to Say When You Talk to Yourself", author: "Shad Helmstetter")
+  ]
+  
+  @Published var images: [Book: Image] = [:]
 }
